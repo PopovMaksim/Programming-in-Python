d30e2046a0990b0753d71fb852ba550f1c104308
PopovMaksim <maksimpopov052006@gmail.com>
2024-09-19 18:56:52 +0300

    lab6 update

--- Лаб6/Task123.py
+++ Лаб6/Task123.py
@@ -1,14 +1,19 @@
-def task1():
-    n = int(input("--Завдання1--\nВведіть кількість елементів у списку:"))
-    list1 = []
-    print("Введіть елементи списку:")
-    for i in range(n):
-        list1.append(None)
-        list1.append(input())
-    print(list1)
+def task1(list1):
+    print("--Завданя 1--\nСписок має вигяд: ", list1)
+    while True:
+        pos = int(input("Введіть непарну позицію, куди необхідно вставити елемент: "))
+        if pos%2==0:
+            print("Позиція парна, спробуйте ще раз!")
+        elif pos>len(list1):
+            print("Позиція перевищує розмір списку! Спробуйте ще раз!")
+        else:
+            break
+
+    list1.insert(pos, input("Введіть що необхідно вставити в список: "))
+    print("Список має вигяд: ", list1)
 
 def task2():
-    n = int(input("--Завдання2--\nВведіть кількість елементів у списку:"))
+    n = int(input("--Завдання 2--\nВведіть кількість елементів у списку:"))
     list2 = []
     print("Введіть елементи списку(тільки числа): ")
     for i in range(n):
@@ -20,11 +25,11 @@ def task2():
     print(list2)
 
 def task3():
-    print("--Завдання3--")
+    print("--Завдання 3--")
     word = input("Введіть слово з латинських літер: ")
     set1 = set(word)
     print("Перші входження: ", set1)
 
-task1()
+task1([0, 1, 2, 3, 4])
 task2()
 task3()
